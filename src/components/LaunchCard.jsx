259a1442d9169c0cbeaa725874949db30a07a6d7
Victoria Lipnitsky <victoria.lipnitsky@gmail.com>
2023-09-16 09:08:23 +0200

    Edited list with launches, styles

--- src/components/LaunchCard.jsx
+++ src/components/LaunchCard.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import "./Launches.css";
 
 const LaunchCard = ({ data }) => {
 
@@ -9,28 +10,44 @@ const LaunchCard = ({ data }) => {
         let exactTime = date.slice(16, 21);
 
         const fullDate = weekDay + ", " + mainDate + " at " + exactTime;
-        
+
         return fullDate;
     }
 
     return (
         <div className="card">
             <div className="row">
-                <div className="col-1"></div>
-                <div className="col-4">
-                    <div>
+                {/* <div className="col-lg-1"></div> */}
+                <div className="col-lg-4 center">
 
-                        <img src={data.links.patch.small
-                            || "https://res.cloudinary.com/devqm7qmb/image/upload/ar_1:1,bo_5px_solid_rgb:000000,c_fill,g_auto,r_max,w_1000/v1694817621/andy-hermawan-bVBvv5xlX3g-unsplash_rmjrqq_902931.jpg"} alt={data.name}></img>
+                    <img src={data.links.patch.small
+                        || "https://res.cloudinary.com/devqm7qmb/image/upload/ar_1:1,bo_5px_solid_rgb:000000,c_fill,g_auto,r_max,w_1000/v1694817621/andy-hermawan-bVBvv5xlX3g-unsplash_rmjrqq_902931.jpg"} alt={data.name}></img>
 
-                    </div>
                 </div>
-                <div className="col-6 launch-info">
-                    <h1>{data.name}</h1>
-                    <p>{data.flight_number}</p>
-                    <p>{changeTimeFormat(data.date_utc)}</p>
+                <div className="col-lg-7 launch-info">
+                    <h3 className="boldString">{data.name}</h3>
+                    <p>
+                        {data.success
+                            ? <span className="successful">successful</span>
+                            : data.upcoming
+                                ? <span className="upcoming">upcoming</span>
+                                : <span className="failed">failed</span>}
+                    </p>
+                    <p>
+                        <a href={`https://www.youtube.com/watch?v=${data.links.youtube_id}`} target="_blank">Youtube</a>
+                        <a href={data.links.wikipedia}>Wikipedia</a>
+                        {data.links.reddit.launch && <a href={data.links.reddit.launch}>Reddit</a>}
+                    </p>
+                    
+                    {(data.failures.length > 0)
+                        ? <p>
+                            <span className="boldString">Reason: </span>{data.failures[0].reason}</p>
+                        : null}
+                    <p>
+                        <span className="boldString">{changeTimeFormat(data.date_utc)}</span>
+                    </p>
                 </div>
-                <div className="col-1"></div>
+                <div className="col-lg-1"></div>
 
             </div>
 
